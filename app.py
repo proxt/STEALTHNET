56dd8b0012f38cecf88987cd4d4f5154d6c83dd1
STEALTHNET <78236639+GOFONCK@users.noreply.github.com>
2025-12-13 14:08:38 +0200

    Update app.py

--- app.py
+++ app.py
@@ -10059,4 +10059,4 @@ def miniapp_static(path):
 if __name__ == '__main__':
     with app.app_context():
         init_database()
-  app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
