328b78fa2ca43099db4feb8797935e9ea1dd12cd
STEALTHNET <78236639+GOFONCK@users.noreply.github.com>
2025-12-26 10:35:56 +0200

    Change app.run to bind to all interfaces

--- app.py
+++ app.py
@@ -11208,5 +11208,4 @@ def miniapp_static(path):
 if __name__ == '__main__':
     with app.app_context():
         init_database()
-    app.run(port=5000, debug=False)
-    app.run(port=5000, debug=False)
+   app.run(host='0.0.0.0', port=5000, debug=False)
